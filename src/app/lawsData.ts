978e80e322e6862666c95dbf07e5cabd4273f550
Gary Saunders <gary.saunders@hunterindustries.com>
2021-10-28 10:18:00 -0700

    Get law image to render

--- src/app/lawsData.ts
+++ src/app/lawsData.ts
@@ -1,14 +1,14 @@
 export const lawsData = [
     {
         'title':'Jakob\'s Law',
-        'image':'img_jakobs_law.png'
+        'image':'./assets/img_jakobs_law.png'
     },
     {
         'title':'Fitt\'s Law',
-        'image':'img_fitts_law.png'
+        'image':'./assets/img_jakobs_law.png'
     },
     {
         'title':'Hick\'s Law',
-        'image':'img_hicks_law.png'
+        'image':'./assets/img_jakobs_law.png'
     },
 ]
